cb013d1e4355335703d2f6be3b17b19f732f6390
lucas jourdain <lucasjourdain25@gmail.com>
2025-06-26 16:16:42 +0200

    [modif]le tableau ne passe pas directement en linkage section et maintenant passe, changement des noms de variables

--- srcs/lirpie.cbl
+++ srcs/lirpie.cbl
@@ -1,8 +1,8 @@
       ******************************************************************
       *                             ENTÊTE                             *
       *                                                                *
-      * lirpie lis                                                     *
-      * Toutes les variables sont à renommer                           *
+      * lirpie lis la database pour récupèrer les pièces et fournisseur*
+      *                                                                *
       *                                                                *
       *                           TRIGRAMMES                           *
       *                                                                *
@@ -12,37 +12,43 @@
        
        IDENTIFICATION DIVISION.
        PROGRAM-ID. lirpie.
-       AUTHOR. .
-       DATE-WRITTEN. .
+       AUTHOR. lucas.
+       DATE-WRITTEN. 26-06-25 (fr).
 
        DATA DIVISION.
        LINKAGE SECTION.
       * Arguments d'entrée.
-       01 TRI                 PIC 9(01).
-           88 TRI-NOM                   VALUE 0.
-           88 TRI-QUANTITE              VALUE 1.
-           88 TRI-FOURNISSEUR           VALUE 2.
+       01 LK-TRI                 PIC 9(01).
+           88 LK-TRI-NOM                   VALUE 0.
+           88 LK-TRI-QUANTITE              VALUE 1.
+           88 LK-TRI-FOURNISSEUR           VALUE 2.
        
-       01 SENS-TRI            PIC 9(01).
-           88 ASCENDANT                 VALUE 0.
-           88 DESCENDANT                VALUE 1.
+       01 LK-SENS-TRI            PIC 9(01).
+           88 LK-ASCENDANT                 VALUE 0.
+           88 LK-DESCENDANT                VALUE 1.
 
-       01 QUANTITE            PIC 9(02). *> Min 1 - Max 25.
+       01 LK-QUANTITE            PIC 9(02). *> Min 1 - Max 25.
 
-       01 PAGE-P              PIC 9(10). *> Min 0 - Max 1,000,000,000.
+       01 LK-PAGE-P              PIC 9(10). *> Min 0 - Max 1,000,000,000.
 
       * Arguments de sortie.
-       01 TABLEAU OCCURS 25 TIMES. *> Max quantité.
-           05 IDENTIFIANT     PIC 9(10).
-           05 NOM             PIC X(80).
-           05 QUANTITE        PIC 9(10).
-           05 SEUIL           PIC 9(10).
-           05 NOM-FOURNISSEUR PIC X(80).
+       01 LK-TABLEAU. 
+           05 LK-TABLEAU-ARGUMENT OCCURS 25 TIMES. *> Max quantité.
+              10 LK-IDENTIFIANT       PIC 9(10).
+              10 LK-NOM               PIC X(80).
+              10 LK-T-QUANTITE        PIC 9(10).
+              10 LK-SEUIL             PIC 9(10).
+              10 LK-NOM-FOURNISSEUR   PIC X(80).
 
-       PROCEDURE DIVISION USING TRI,
-                                SENS-TRI,
-                                QUANTITE,
-                                PAGE-P,
-                                TABLEAU.
+       PROCEDURE DIVISION USING LK-TRI,
+                                LK-SENS-TRI,
+                                LK-QUANTITE,
+                                LK-PAGE-P,
+                                LK-TABLEAU.
+
+
+           EXEC SQL
+                CONNECT :USERNAME IDENTIFIED BY :PASSWD USING :DBNAME
+           END-EXEC.
 
            EXIT PROGRAM.
