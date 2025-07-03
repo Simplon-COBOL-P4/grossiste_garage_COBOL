75dc5b6a3e5003aaf8912dd803016329b68e7e5e
sibory-gueye <sibory.gueye@gmail.com>
2025-07-03 13:57:04 +0200

    [ajout] Ajout du sous-programme 'ajupie'

--- srcs/ajupie.cbl
+++ srcs/ajupie.cbl
@@ -1,62 +1,111 @@
       ******************************************************************
       *                             ENTÊTE                             *
+      *                                                                *
       *    Le programme principal qui va appeler le sous-programme     *
       *    'spajupie' pour l'ajout de pièces dans la BDD               *
       *                                                                *
       *                           TRIGRAMMES                           *
       *                                                                *
-      *    AJOUT=AJU; PIE=PIECE; QTE=QUANTITE; MIN=MINIMUM; *
-      *    FOU=FOURNISSEUR; APL=APPEL;                                 *
-      ******************************************************************
+      *    AJU=AJOUT; PIE=PIECE; QTE=QUANTITE; MIN=MINIMUM;            *
+      *    FOU=FOURNISSEUR; APL=APPEL; CNX=CONNEXION;                  *
+      *    SP=SOUS PROGRAMME; UTI=UTILISATEUR; COM=COMMIT              *
+      *                                                                *
+      ****************************************************************** 
        
        IDENTIFICATION DIVISION.
        PROGRAM-ID. ajupie.
        AUTHOR. siboryg.
-       DATE-WRITTEN. 26-06-2025 (fr).
+       DATE-WRITTEN. 03-07-2025 (fr).
 
        DATA DIVISION.
-      *    Déclaration des variables 
        WORKING-STORAGE SECTION.
-       01  WS-PIE-NOM                   PIC X(80).
-       01  WS-PIE-QTE                   PIC 9(10).
-       01  WS-PIE-MIN                   PIC 9(10).
-       01  WS-ID-FOU                    PIC 9(10).
+
+      *    Déclaration des variables pour le EXEC SQL.
+       EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+       01  PG-PIE-NOM                   PIC X(80).
+       01  PG-PIE-QTE                   PIC 9(10).
+       01  PG-PIE-MIN                   PIC 9(10).
+       01  PG-ID-FOU                    PIC 9(10).      
+    
+    
+      *    Déclaration des variables pour la connexion à la BDD.
+       01  PG-UTI                       PIC X(30) VALUE "postgres".
+       01  PG-MDP                       PIC X(30) VALUE "mdp".
+       01  PG-BDD                       PIC X(10) VALUE "logiparts".
+       EXEC SQL END DECLARE SECTION END-EXEC.
+       EXEC SQL INCLUDE SQLCA END-EXEC.
+
+       LINKAGE SECTION.
+      * Arguments d'entrée.
+       01  LK-PIE-NOM                   PIC X(80).
+       01  LK-PIE-QTE                   PIC 9(10).
+       01  LK-PIE-MIN                   PIC 9(10).
+       01  LK-ID-FOU                    PIC 9(10).
+
+       PROCEDURE DIVISION USING LK-PIE-NOM,
+                                LK-PIE-QTE,
+                                LK-PIE-MIN,
+                                LK-ID-FOU.
 
 
-       PROCEDURE DIVISION.
-      *    Enregistrement des données pour tester l'ajout
-           DISPLAY "Saisie du nom de la pièce :".
-           ACCEPT WS-PIE-NOM.
-
-           DISPLAY "Saisie de la quantité de pièces :".
-           ACCEPT WS-PIE-QTE.
-           MOVE FUNCTION NUMVAL(WS-PIE-QTE) TO WS-PIE-QTE.
-
-           DISPLAY "Saisie du seuil minimum de pièces :".
-           ACCEPT WS-PIE-MIN.
-           MOVE FUNCTION NUMVAL(WS-PIE-MIN) TO WS-PIE-MIN.
-
-           DISPLAY "Saisie de l'ID du fournisseur :".
-           ACCEPT WS-ID-FOU.
-           MOVE FUNCTION NUMVAL(WS-ID-FOU) TO WS-ID-FOU.
-
-      *    Paragraphe pour l'ajout des pièces
-           PERFORM 0100-APL-PIE-DEB
-              THRU 0100-APL-PIE-FIN.
+      *    Paragraphe pour la connexion à la BDD. 
+           PERFORM 0100-CNX-BDD-DEB
+              THRU 0100-CNX-BDD-FIN.
            
-           STOP RUN.
+      *    Paragraphe pour l'ajout de pièces à la BDD.
+           PERFORM 0200-AJU-PIE-DEB
+              THRU 0200-AJU-PIE-FIN.
 
-      *    Paragraphe pour l'ajout des pièces, notamment avec la 
-      *    fonction CALL
-       0100-APL-PIE-DEB.
-           CALL 'spajupie' 
-               USING WS-PIE-NOM
-                     WS-PIE-QTE 
-                     WS-PIE-MIN 
-                     WS-ID-FOU
-           END-CALL.
-
-      *    Sortie de paragraphe
-       0100-APL-PIE-FIN.
-           EXIT.
+      *    Paragraphe pour le commit.
+           PERFORM 0300-COM-DEB
+              THRU 0300-COM-FIN.
        
+           EXIT PROGRAM.
+
+      *    Paragraphe pour la connexion, génère un SQLCODE pour les
+      *    erreurs.
+       0100-CNX-BDD-DEB.
+           EXEC SQL
+                CONNECT :PG-UTI IDENTIFIED BY :PG-MDP 
+                USING :PG-BDD
+           END-EXEC.
+              IF SQLCODE NOT EQUAL 0
+                   EXIT PROGRAM
+                   END-IF.
+
+      *    Sortie de paragraphe.
+       0100-CNX-BDD-FIN.
+           EXIT.
+
+      *    Paragraphe pour l'ajout de pièces.
+       0200-AJU-PIE-DEB.         
+
+           MOVE LK-PIE-NOM TO PG-PIE-NOM.
+           MOVE LK-PIE-QTE TO PG-PIE-QTE.
+           MOVE LK-PIE-MIN TO PG-PIE-MIN.
+           MOVE LK-ID-FOU  TO PG-ID-FOU.
+      *    La requête SQL pour l'ajout de pièces.
+           EXEC SQL 
+           INSERT INTO piece (nom_pie, qt_pie, seuil_pie, id_fou)
+           VALUES (
+               :PG-PIE-NOM, 
+               :PG-PIE-QTE, 
+               :PG-PIE-MIN,
+               :PG-ID-FOU
+               )
+           END-EXEC.
+
+      *    Sortie de parapgraphe.
+       0200-AJU-PIE-FIN.
+           EXIT.
+      
+      *    Parapgraphe COMMIT pour la requête SQL.
+       0300-COM-DEB.
+           EXEC SQL COMMIT END-EXEC.
+           IF SQLCODE NOT EQUAL 0
+                   EXIT PROGRAM
+                   END-IF.
+
+      *    Sortie de paragraphe.
+       0300-COM-FIN.
+           EXIT.
--- srcs/spajupie.cbl
+++ srcs/spajupie.cbl
@@ -1,114 +1,62 @@
       ******************************************************************
       *                             ENTÊTE                             *
-      *    C'est un sous-programme qui ajoute des pièces à la base de  *
-      *    données.                                                    *
+      *    C'est un programme qui appelle 'ajupie' pour ajouter des    *
+      *    pièces à la base de données.                                *
       *                                                                *
       *                                                                *
       *                           TRIGRAMMES                           *
       *                                                                *
-      *    AJU=AJOUT; PIE=PIECE; QTE=QUANTITE; MIN=MINIMUM; *
-      *    FOU=FOURNISSEUR; APL=APPEL; CNX=CONNEXION;                                 *
-      ****************************************************************** 
+      *    AJOUT=AJU; PIE=PIECE; QTE=QUANTITE; MIN=MINIMUM;            *
+      *    FOU=FOURNISSEUR; APL=APPEL;                                 *
+      ******************************************************************
        
        IDENTIFICATION DIVISION.
        PROGRAM-ID. spajupie.
        AUTHOR. siboryg.
-       DATE-WRITTEN. 26-06-2025 (fr).
+       DATE-WRITTEN. 03-07-2025 (fr).
 
        DATA DIVISION.
+      *    Déclaration des variables. 
        WORKING-STORAGE SECTION.
+       01  WS-PIE-NOM                   PIC X(80).
+       01  WS-PIE-QTE                   PIC 9(10).
+       01  WS-PIE-MIN                   PIC 9(10).
+       01  WS-ID-FOU                    PIC 9(10).
 
-      *    Déclaration des variables pour le EXEC SQL
-       EXEC SQL BEGIN DECLARE SECTION END-EXEC.
-       01  PG-PIE-NOM                   PIC X(80).
-       01  PG-PIE-QTE                   PIC 9(10).
-       01  PG-PIE-MIN                   PIC 9(10).
-       01  PG-ID-FOU                    PIC 9(10).      
-    
-    
-      *    Déclaration des variables pour la connexion à la BDD
-       01  PG-UTILISATEUR               PIC X(30) VALUE "postgres".
-       01  PG-MDP                       PIC X(30) VALUE "mdp".
-       01  PG-BDD                       PIC X(10) VALUE "logiparts".
-       EXEC SQL END DECLARE SECTION END-EXEC.
-       EXEC SQL INCLUDE SQLCA END-EXEC.
+       PROCEDURE DIVISION.
+      *    Enregistrement des données pour tester l'ajout.
+           DISPLAY "Saisie du nom de la pièce :".
+           ACCEPT WS-PIE-NOM.
 
-       LINKAGE SECTION.
-      * Arguments d'entrée.
-       01  LK-PIE-NOM                   PIC X(80).
-       01  LK-PIE-QTE                   PIC 9(10).
-       01  LK-PIE-MIN                   PIC 9(10).
-       01  LK-ID-FOU                    PIC 9(10).
+           DISPLAY "Saisie de la quantité de pièces :".
+           ACCEPT WS-PIE-QTE.
+           MOVE FUNCTION NUMVAL(WS-PIE-QTE) TO WS-PIE-QTE.
 
-       PROCEDURE DIVISION USING LK-PIE-NOM,
-                                LK-PIE-QTE,
-                                LK-PIE-MIN,
-                                LK-ID-FOU.
+           DISPLAY "Saisie du seuil minimum de pièces :".
+           ACCEPT WS-PIE-MIN.
+           MOVE FUNCTION NUMVAL(WS-PIE-MIN) TO WS-PIE-MIN.
 
-      *    Paragraphra pour la connexion à la BDD 
-           PERFORM 0100-CNX-BDD-DEB
-              THRU 0100-CNX-BDD-FIN.
+           DISPLAY "Saisie de l'ID du fournisseur :".
+           ACCEPT WS-ID-FOU.
+           MOVE FUNCTION NUMVAL(WS-ID-FOU) TO WS-ID-FOU.
+
+      *    Paragraphe pour l'ajout des pièces.
+           PERFORM 0100-APL-PIE-DEB
+              THRU 0100-APL-PIE-FIN.
            
-      *    Paragraphe pour l'ajout de pièces à la BDD
-           PERFORM 0200-AJU-PIE-DEB
-              THRU 0200-AJU-PIE-FIN.
-       
-           EXIT PROGRAM.
+           STOP RUN.
 
-      *    Paragraphe pour la connexion, génère un SQLCODE pour les
-      *    erreurs
-       0100-CNX-BDD-DEB.
-           EXEC SQL
-                CONNECT :PG-UTILISATEUR IDENTIFIED BY :PG-MDP 
-                USING :PG-BDD
-           END-EXEC.
-              IF SQLCODE = 0
-                   DISPLAY "Connexion réussie !"
-               ELSE
-                   DISPLAY "Erreur de connexion SQLCODE: " SQLCODE
-                   EXIT PROGRAM
-               END-IF.
+      *    Paragraphe pour l'ajout des pièces, notamment avec la 
+      *    fonction CALL.
+       0100-APL-PIE-DEB.
+           CALL 'ajupie' 
+               USING WS-PIE-NOM
+                     WS-PIE-QTE 
+                     WS-PIE-MIN 
+                     WS-ID-FOU
+           END-CALL.
 
-      *    Sortie de paragraphe
-       0100-CNX-BDD-FIN.
-           EXIT.
-
-      *    Paragraphe pour l'ajout de pièces
-       0200-AJU-PIE-DEB.         
-
-           MOVE LK-PIE-NOM TO PG-PIE-NOM.
-           MOVE LK-PIE-QTE TO PG-PIE-QTE.
-           MOVE LK-PIE-MIN TO PG-PIE-MIN.
-           MOVE LK-ID-FOU  TO PG-ID-FOU.
-
-      *    La requête SQL pour l'ajout de pièces
-           EXEC SQL 
-           INSERT INTO piece (nom_pie, qt_pie, seuil_pie, id_fou)
-           VALUES (
-               :PG-PIE-NOM, 
-               :PG-PIE-QTE, 
-               :PG-PIE-MIN,
-               :PG-ID-FOU
-               )
-           END-EXEC.
-               IF SQLCODE = 0
-                   DISPLAY "Insertion réussie !"
-               ELSE
-                   DISPLAY "Erreur d'insertion SQLCODE: " SQLCODE
-               END-IF.
-           
-
-
-      *    La COMMIT pour la requête SQL
-           EXEC SQL COMMIT END-EXEC.
-           IF SQLCODE = 0
-                   DISPLAY "Commit réussi !"
-               ELSE
-                   DISPLAY "Erreur commit SQLCODE: " SQLCODE
-                   EXIT PROGRAM
-               END-IF.
-
-      *    Sortie de parapgraphe
-       0200-AJU-PIE-FIN.
+      *    Sortie de paragraphe.
+       0100-APL-PIE-FIN.
            EXIT.
       
