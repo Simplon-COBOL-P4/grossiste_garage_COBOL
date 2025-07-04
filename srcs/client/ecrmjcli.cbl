a36d169d4da72123d1533e36ca88e843d05017a1
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-04 16:40:43 +0200

    [modif] Changement logique, menu qui boucle

--- srcs/client/ecrmjcli.cbl
+++ srcs/client/ecrmjcli.cbl
@@ -38,43 +38,41 @@
            05 LINE 06 COLUMN 50 VALUE "]".
 
            05 LINE 08 COLUMN 03 VALUE "Nom : ".
-           05 LINE 09 COLUMN 04 PIC X(50) TO WS-NOM-CLI.
+           05 LINE 09 COLUMN 04 PIC X(50) FROM WS-NOM-CLI.
            05 LINE 09 COLUMN 03 VALUE "[".
            05 LINE 09 COLUMN 54 VALUE "]".
 
            05 LINE 10 COLUMN 03 VALUE "Email : ".
-           05 LINE 11  COLUMN 04 PIC X(50) TO WS-EML-CLI.
+           05 LINE 11  COLUMN 04 PIC X(50) FROM WS-EML-CLI.
            05 LINE 11 COLUMN 03 VALUE "[".
            05 LINE 11 COLUMN 54 VALUE "]".
 
            05 LINE 12 COLUMN 03 VALUE "Indicatif / Telephone : ".
-           05 LINE 13 COLUMN 05 PIC Z(03) TO WS-IND-CLI.
-           05 COLUMN 09 PIC Z(10) TO WS-TEL-CLI AUTO.
+           05 LINE 13 COLUMN 05 PIC Z(03) FROM WS-IND-CLI.
+           05 COLUMN 09 PIC Z(10) FROM WS-TEL-CLI AUTO.
            05 LINE 13 COLUMN 03 VALUE "[+".
            05 LINE 13 COLUMN 19 VALUE "]".
 
            05 LINE 14 COLUMN 03 VALUE "Adresse : ".
-           05 LINE 15 COLUMN 04 PIC X(50) TO WS-ADR-CLI.
+           05 LINE 15 COLUMN 04 PIC X(50) FROM WS-ADR-CLI.
            05 LINE 15 COLUMN 03 VALUE "[".
            05 LINE 15 COLUMN 54 VALUE "]".
 
            05 LINE 16 COLUMN 03 VALUE "Ville : ".
-           05 LINE 17 COLUMN 04 PIC X(50) TO WS-VIL-CLI.
+           05 LINE 17 COLUMN 04 PIC X(50) FROM WS-VIL-CLI.
            05 LINE 17 COLUMN 03 VALUE "[".
            05 LINE 17 COLUMN 54 VALUE "]".
 
            05 LINE 18 COLUMN 03 VALUE "Code postal : ".
-           05 LINE 19 COLUMN 04 PIC Z(05) TO WS-CP-CLI.
+           05 LINE 19 COLUMN 04 PIC Z(05) FROM WS-CP-CLI.
            05 LINE 19 COLUMN 03 VALUE "[".
            05 LINE 19 COLUMN 09 VALUE "]".
 
-           05 LINE 20 COLUMN 30 VALUE "Confirmer modifications ?".
-           05 LINE 22 COLUMN 30 VALUE "1 - Oui  0 - Annuler ".
+           05 LINE 20 COLUMN 30 VALUE "Chercher client ?".
+           05 LINE 22 COLUMN 30 VALUE "1 - Oui  0 - Retour ".
            05 LINE 23 COLUMN 41 PIC X(01) TO WS-CHX-ADM.
            05 LINE 23 COLUMN 40 VALUE "[".
            05 LINE 23 COLUMN 42 VALUE "]".
-
-           
 
       * Écran pour afficher les infos de client récupérées avant 
       * modification.
@@ -86,8 +84,9 @@
            05 LINE 15 COLUMN 04 PIC X(50) USING WS-ADR-CLI.
            05 LINE 17 COLUMN 04 PIC X(50) USING WS-VIL-CLI.
            05 LINE 19 COLUMN 04 PIC 9(05) USING WS-CP-CLI.
-           05 LINE 23 COLUMN 41 PIC X(01) USING WS-CHX-ADM.
-
+           05 LINE 20 COLUMN 30 VALUE "Confirmer modifications ?".
+           05 LINE 22 COLUMN 30 VALUE "1 - Oui  2 - Non  0 - Retour ".
+           05 LINE 23 COLUMN 41 PIC X(01) TO WS-CHX-ADM.
      
 
       ******************************************************************
@@ -100,45 +99,53 @@
       *               Lecture et modification du client                *
       ******************************************************************
        0100-MAJ-CLI-DEB.
+           MOVE "1" TO WS-CHX-ADM
+           PERFORM UNTIL WS-CHX-ADM EQUAL 0
       * On affiche l'écran pour saisir un id de client.
-           DISPLAY S-FND-ECR
-           DISPLAY S-ECR-CLI
-           ACCEPT  S-ECR-CLI
-
-           CALL "liridcli"
-               USING
-               WS-IDT-CLI
-               WS-NOM-CLI
-               WS-EML-CLI
-               WS-IND-CLI
-               WS-TEL-CLI
-               WS-CP-CLI
-               WS-VIL-CLI
-               WS-ADR-CLI
-           END-CALL.                      
-                                
-
-           PERFORM UNTIL WS-CHX-ADM = "1" OR WS-CHX-ADM = "0"
-
-      * Si le client existe on affiche ses info en on peux les modifier.     
-              DISPLAY S-ECR-AFG
-              ACCEPT  S-ECR-AFG
-      
+               DISPLAY S-FND-ECR
+               DISPLAY S-ECR-CLI
+               MOVE "0" TO WS-CHX-ADM
+               PERFORM UNTIL WS-CHX-ADM > "0"
+                   ACCEPT S-ECR-CLI
+                   IF WS-CHX-ADM EQUAL 0
+                       EXIT PROGRAM
+                   END-IF
+               END-PERFORM
+            
+               CALL "liridcli"
+                   USING
+                   WS-IDT-CLI
+                   WS-NOM-CLI
+                   WS-EML-CLI
+                   WS-IND-CLI
+                   WS-TEL-CLI
+                   WS-CP-CLI
+                   WS-VIL-CLI
+                   WS-ADR-CLI
+               END-CALL                      
+                                    
+               DISPLAY S-ECR-AFG
+               MOVE "0"
+               PERFORM UNTIL WS-CHX-ADM > "0"
+                   ACCEPT  S-ECR-AFG
+                   IF WS-CHX-ADM EQUAL 0
+                       EXIT PROGRAM
+                   END-IF
+               END-PERFORM
       * Si l'admin tape "1" on appelle le programme (majcli) pour
       * modifier.
-              IF WS-CHX-ADM = "1"
-                  CALL "majcli"
-                      USING
-                      WS-IDT-CLI
-                      WS-NOM-CLI
-                      WS-EML-CLI
-                      WS-IND-CLI
-                      WS-TEL-CLI
-                      WS-CP-CLI
-                      WS-VIL-CLI
-                      WS-ADR-CLI
-                  END-CALL                  
-                                     
+               IF WS-CHX-ADM = "1"
+                   CALL "majcli"
+                       USING
+                       WS-IDT-CLI
+                       WS-NOM-CLI
+                       WS-EML-CLI
+                       WS-IND-CLI
+                       WS-TEL-CLI
+                       WS-CP-CLI
+                       WS-VIL-CLI
+                       WS-ADR-CLI
+                   END-CALL
                END-IF
            END-PERFORM.                        
 
