9ebf0e4a9edd428bc7f2e106adedc02c55acd32f
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-04 14:11:22 +0200

    [ajout] Oubli d'appel d'un sous programme

--- srcs/piece/ecrmjinf.cbl
+++ srcs/piece/ecrmjinf.cbl
@@ -20,15 +20,32 @@
        01 NOM                  PIC X(50).
        01 SEUIL                PIC 9(10).
        01 ID-FOUR              PIC 9(10).
+       01 POUBELLE             PIC X(50).
 
        PROCEDURE DIVISION.
-       
+
+           CALL "liridpie"
+               USING
+      * Arguments d'entrée
+               IDENTIFIANT
+      * Fin des arguments d'entrée
+      * Début des arguments de sortie
+               NOM
+               SEUIL
+               ID-FOUR
+      * Argument non utilisé, obligé de le mettre quand même.
+               POUBELLE
+      * Fin des arguments de sortie
+           END-CALL.
+
            CALL "mjinfpie"
                USING
+      * Arguments d'entrée
                IDENTIFIANT
                NOM
                SEUIL
                ID-FOUR
+      * Fin des arguments d'entrée
            END-CALL.
 
            EXIT PROGRAM.
