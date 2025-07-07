5b9ff8de34dca11824b1f2edb5a9002691384715
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-07 12:15:02 +0200

    [ajout] Création structure

---
+++ srcs/fournisseur/majfou.cbl
@@ -0,0 +1,43 @@
+      ******************************************************************
+      *                             ENTÊTE                             *
+      *                                                                *
+      *                                                                *
+      *                                                                *
+      *                           TRIGRAMMES                           *
+      * LIR=LIRE; NM=NOM; FOU=FOURNISSEUR;                             *
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. majfou.
+       AUTHOR. .
+       DATE-WRITTEN. .
+
+       DATA DIVISION.
+       LINKAGE SECTION.
+      * Arguments d'entrée.
+       01 IDENTIFIANT           PIC 9(10).
+       01 NOM                   PIC X(50).
+       01 ADRESSE               PIC X(50).
+       01 VILLE                 PIC X(50).
+       01 CODE-POSTAL           PIC 9(05).
+       01 INDICATIF             PIC 9(03).
+       01 TELEPHONE             PIC 9(10).
+       01 EMAIL                 PIC X(50).
+
+      * Arguments de sortie.
+
+       COPY majret REPLACING ==:PREFIX:== BY ==LK==.
+
+       PROCEDURE DIVISION USING IDENTIFIANT,
+                                NOM,
+                                ADRESSE,
+                                VILLE,
+                                CODE-POSTAL,
+                                INDICATIF,
+                                TELEPHONE,
+                                EMAIL,
+                                LK-MAJ-RET.
+
+           EXIT PROGRAM.
