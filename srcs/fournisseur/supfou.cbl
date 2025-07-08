c3b1066bf21929bc8c2cdb6de54d5348bb22ac27
Thomas6A <thomas.baudrin@outlook.fr>
2025-07-08 16:59:24 +0200

    [ajout] suppression d'un fournisseur

--- srcs/fournisseur/supfou.cbl
+++ srcs/fournisseur/supfou.cbl
@@ -1,29 +1,73 @@
       ******************************************************************
       *                             ENTÊTE                             *
       *                                                                *
-      *                                                                *
-      *                                                                *
-      *                           TRIGRAMMES                           *
-      * SUP=SUPPRIMER; FOU=FOURNISSEUR;                                *
-      *                                                                *
-      *                                                                *
+      *** TRIGRAMMES:                                                  *
+      * SUPPRIMER=SUP; FOURNISSEUR=FOU; IDENTIFIANT=ID; DETAIL=DET;    *
+      * UTILISATEUR=UTI                                                *
+      *** FONCTION DU PROGRAMME:                                       *
+      * IL SUPPRIME UN FOURNISSEUR PAR SON ID DANS LA TABLE            * 
+      * 'fournisseur'                                                  *
       ******************************************************************
        
        IDENTIFICATION DIVISION.
        PROGRAM-ID. supfou.
-       AUTHOR. .
-       DATE-WRITTEN. .
+       AUTHOR. Thomas Baudrin.
+       DATE-WRITTEN. 08-07-2025 (fr).
 
        DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+       01  PG-ID-FOU         PIC 9(10).
+       EXEC SQL END DECLARE SECTION END-EXEC.
+       77  WS-LOG-DET        PIC X(100).
+       EXEC SQL INCLUDE SQLCA END-EXEC.
+
        LINKAGE SECTION.
       * Arguments d'entrée.
-       01 IDENTIFIANT           PIC 9(10).
-
-      * Arguments de sortie.
+       01 LK-ID-FOU         PIC 9(10).
+       01 LK-UTI-ID         PIC 9(10).
 
        COPY supret REPLACING ==:PREFIX:== BY ==LK==.
 
-       PROCEDURE DIVISION USING IDENTIFIANT,
-                                LK-SUP-RET.
+       PROCEDURE DIVISION USING LK-ID-FOU,
+                                LK-SUP-RET,
+                                LK-UTI-ID.
+
+      * SUPPRIME UN FOURNISSEUR.
+           PERFORM 0100-SUP-FOU-DEB
+              THRU 0100-SUP-FOU-FIN.
 
            EXIT PROGRAM.
+
+
+      ******************************************************************
+      *                          PARAGRAPHES                           * 
+      ******************************************************************
+       0100-SUP-FOU-DEB.
+           MOVE LK-ID-FOU   TO PG-ID-FOU.
+           
+           EXEC SQL
+               DELETE FROM fournisseur
+               WHERE id_fou = :PG-ID-FOU
+           END-EXEC.
+           IF SQLCODE = 0
+      * Le fournisseur est supprimé avec succès.
+               EXEC SQL COMMIT END-EXEC
+               STRING "["
+                      LK-ID-FOU
+                      "] Suppresion"
+                      INTO WS-LOG-DET
+               END-STRING
+               CALL "crelog"
+                     USING 
+                     WS-LOG-DET
+                     "Fournisseur"
+                     LK-UTI-ID
+               END-CALL
+               SET LK-SUP-RET-OK       TO TRUE       
+           ELSE
+      * Le fournisseur n'est pas dans la table ou la table n'existe pas.
+               EXEC SQL ROLLBACK END-EXEC
+               SET LK-SUP-RET-ERR      TO TRUE
+           END-IF.
+       0100-SUP-FOU-FIN.
