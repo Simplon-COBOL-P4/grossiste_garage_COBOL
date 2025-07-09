f6513c58a8904fd9165085882fe312c32787c4c5
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-09 11:55:46 +0200

    [ajout] Création structure

---
+++ srcs/livraison/ecrmjliv.cbl
@@ -0,0 +1,82 @@
+      ******************************************************************
+      *                             ENTÊTE                             *
+      *                                                                *
+      *                                                                *
+      *                                                                *
+      *                           TRIGRAMMES                           *
+      * ECR=ECRAN; MJ=MISE A JOUR; LIV=LIVRAISON;                      *
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ecrmjliv.
+       AUTHOR. .
+       DATE-WRITTEN. .
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 IDENTIFIANT             PIC 9(10).
+       01 WS-DATE                 PIC X(10).
+       01 STATUT                  PIC 9(01).
+           88 STATUT-EN-COURS               VALUE 0.
+           88 STATUT-TERMINE                VALUE 1.
+       01 TYPE                    PIC 9(01).
+           88 TYPE-ENTRANTE                 VALUE 0.
+           88 TYPE-SORTANTE                 VALUE 1.
+      * Identifiant et nom fournisseur si entrante,
+      * et client si sortante.
+       01 IDENTIFIANT-SORTIE      PIC 9(10).
+       01 NOM-SORTIE              PIC X(50).
+
+       COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
+       COPY majret REPLACING ==:PREFIX:== BY ==WS==.
+
+       01 IDENTIFIANT-PIECE       PIC 9(10).
+       01 QUANTITE-PIECE          PIC 9(10).
+       01 TYPE-CHANGEMENT         PIC 9(01).
+           88 CHANGEMENT-AJOUT              VALUE 0.
+           88 CHANGEMENT-RETRAIT            VALUE 1.
+
+      *SCREEN SECTION.
+      *COPY ecrprn.
+
+       PROCEDURE DIVISION.
+
+           CALL "liridliv"
+               USING
+      * Arguments d'entrée
+               IDENTIFIANT
+      * Fin des arguments d'entrée
+      * Début des arguments de sortie
+               WS-DATE
+               STATUT
+               TYPE
+               IDENTIFIANT-SORTIE
+               NOM-SORTIE
+               WS-LIR-RET
+      * Fin des arguments de sortie
+           END-CALL.
+
+           CALL "majliv"
+               USING
+      * Arguments d'entrée
+               IDENTIFIANT
+      * Fin des arguments d'entrée
+      * Début des arguments de sortie
+               WS-MAJ-RET
+      * Fin des arguments de sortie
+           END-CALL.
+
+           IF TYPE-ENTRANTE THEN
+               PERFORM
+                   CALL "majpie"
+                       USING
+                       IDENTIFIANT-PIECE
+                       QUANTITE-PIECE
+                       TYPE-CHANGEMENT
+                   END-CALL
+               END-PERFORM
+           END-IF.
+
+           EXIT PROGRAM.
