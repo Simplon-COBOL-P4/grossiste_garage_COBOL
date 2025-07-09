867e8643ef0fd99ebf90eda3a6c9ac023fc1c5fc
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-09 13:42:28 +0200

    [modif] Modification squelette, rajout appel sous programme fetlivpi

--- srcs/livraison/ecrmjliv.cbl
+++ srcs/livraison/ecrmjliv.cbl
@@ -16,7 +16,7 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 IDENTIFIANT             PIC 9(10).
+       01 IDENTIFIANT-LIVRAISON   PIC 9(10).
        01 WS-DATE                 PIC X(10).
        01 STATUT                  PIC 9(01).
            88 STATUT-EN-COURS               VALUE 0.
@@ -32,6 +32,10 @@
        COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
        COPY majret REPLACING ==:PREFIX:== BY ==WS==.
 
+       01 RETOUR-LECTURE          PIC 9(01).
+           88 RETOUR-DONNEE-LUE                VALUE 0.
+           88 RETOUR-FIN-LECTURE               VALUE 1.
+
        01 IDENTIFIANT-PIECE       PIC 9(10).
        01 QUANTITE-PIECE          PIC 9(10).
        01 TYPE-CHANGEMENT         PIC 9(01).
@@ -46,7 +50,7 @@
            CALL "liridliv"
                USING
       * Arguments d'entrée
-               IDENTIFIANT
+               IDENTIFIANT-LIVRAISON
       * Fin des arguments d'entrée
       * Début des arguments de sortie
                WS-DATE
@@ -61,7 +65,7 @@
            CALL "majliv"
                USING
       * Arguments d'entrée
-               IDENTIFIANT
+               IDENTIFIANT-LIVRAISON
       * Fin des arguments d'entrée
       * Début des arguments de sortie
                WS-MAJ-RET
@@ -70,6 +74,18 @@
 
            IF TYPE-ENTRANTE THEN
                PERFORM
+                   CALL "fetlivpi"
+                       USING
+      * Arguments d'entrée
+                       IDENTIFIANT-LIVRAISON
+      * Fin des arguments d'entrée
+      * Début des arguments de sortie
+                       IDENTIFIANT-PIECE
+                       QUANTITE-PIECE
+                       RETOUR-LECTURE
+      * Fin des arguments de sortie
+                   END-CALL
+
                    CALL "majpie"
                        USING
                        IDENTIFIANT-PIECE
