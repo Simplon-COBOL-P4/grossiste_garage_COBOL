6fe07a06c198ec62070c867571b18274f01355bc
lucas jourdain <lucasjourdain25@gmail.com>
2025-07-09 15:15:40 +0200

    [ajout] ajout de livraison dans la bdd + logs seulement quand ajout réussi

--- srcs/livraison/ajuliv.cbl
+++ srcs/livraison/ajuliv.cbl
@@ -1,38 +1,117 @@
       ******************************************************************
       *                             ENTÊTE                             *
-      *                                                                *
+      *  Programme qui ajoute une livraison à la BDD.                  *
       *                                                                *
       *                                                                *
       *                           TRIGRAMMES                           *
-      * AJU=AJOUT; LIV=LIVRAISON;                                      *
-      *                                                                *
-      *                                                                *
+      * AJU=AJOUT; LIV=LIVRAISON; STA=statut; DAT=date; TYP=type;      *
+      * IDN=identifiant; INI=initialisation; VAR=variable;             *
+      * DTL=détail; LG=log; UTI=UTILISATEUR; ENT=entrante;             *
+      * SOR=sortante; CRS=cours                                        * 
       ******************************************************************
        
        IDENTIFICATION DIVISION.
        PROGRAM-ID. ajuliv.
-       AUTHOR. .
-       DATE-WRITTEN. .
+       AUTHOR. lucas.
+       DATE-WRITTEN. 08-07-2025 (fr).
 
        DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+       01 PG-DAT-DEB                    PIC X(10).
+       01 PG-DAT-FIN                    PIC X(10).
+       01 PG-STA                        PIC 9(01).
+       01 PG-IDN                        PIC 9(10).
+       EXEC SQL END DECLARE SECTION END-EXEC.
+       EXEC SQL INCLUDE SQLCA END-EXEC.
+
+      * variable pour créer le log
+       01 WS-DTL-LG                     PIC X(100).
+       01 WS-TYP-LG                     PIC X(12).
+       01 WS-UTI-ID                     PIC 9(10) VALUE 1.
+
+
        LINKAGE SECTION.
       * Arguments d'entrée.
-       01 DATE                    PIC X(10).
-       01 STATUT                  PIC 9(01).
-           88 STATUT-EN-COURS               VALUE 0.
-           88 STATUT-TERMINE                VALUE 1.
-       01 TYPE                    PIC 9(01).
-           88 TYPE-ENTRANTE                 VALUE 0.
-           88 TYPE-SORTANTE                 VALUE 1.
+       01 LK-DAT-DEB              PIC X(10).
+       01 LK-DAT-FIN              PIC X(10).
+       01 LK-STA                  PIC 9(01).
+           88 LK-STA-EN-CRS                 VALUE 0.
+           88 LK-STA-FIN                    VALUE 1.
+       01 LK-TYP                  PIC 9(01).
+           88 LK-TYP-ENT                    VALUE 0.
+           88 LK-TYP-SOR                    VALUE 1.
       * Identifiant fournisseur si entrante, et client si sortante.
-       01 IDENTIFIANT             PIC 9(10).
+       01 LK-IDN                  PIC 9(10).
       * Arguments de sortie.
        COPY ajuret REPLACING ==:PREFIX:== BY ==LK==.
 
-       PROCEDURE DIVISION USING DATE,
-                                STATUT,
-                                TYPE,
-                                IDENTIFIANT,
+
+       PROCEDURE DIVISION USING LK-DAT-DEB,
+                                LK-DAT-FIN, 
+                                LK-STA,
+                                LK-TYP,
+                                LK-IDN,
                                 LK-AJU-RET.
 
+
+           PERFORM 0100-INI-VAR-DEB
+              THRU 0100-INI-VAR-FIN.
+
+           PERFORM 0200-SQL-DEB
+              THRU 0200-SQL-FIN.
+
            EXIT PROGRAM.
+
+       0100-INI-VAR-DEB.
+      * on met les valeurs de la linkage dans la working.
+           MOVE LK-DAT-DEB        TO PG-DAT-DEB.
+           MOVE LK-DAT-FIN        TO PG-DAT-FIN.
+           MOVE LK-STA            TO PG-STA.
+           MOVE LK-IDN            TO PG-IDN.
+       0100-INI-VAR-FIN.
+
+       0200-SQL-DEB.
+
+      * Si on a l'identificant du client.
+           IF LK-TYP EQUAL 1
+               EXEC SQL
+               INSERT INTO livraison(date_deb_liv, date_fin_liv, 
+               statut_liv, id_cli)
+               VALUES(:PG-DAT-DEB, :PG-DAT-FIN, 
+               :PG-STA, :PG-IDN)
+               END-EXEC
+
+      * Si on a l'identificant du fournisseur.
+           ELSE 
+               EXEC SQL
+               INSERT INTO livraison(date_deb_liv, date_fin_liv, 
+               statut_liv, id_fou)
+               VALUES(:PG-DAT-DEB, :PG-DAT-FIN, 
+               :PG-STA, :PG-IDN)
+               END-EXEC
+
+           END-IF.
+
+           IF SQLCODE = 0
+      * On rajoute, pour le log, si l'ajout dans la bdd est un succès 
+      * ou non.     
+               SET LK-AJU-RET-OK TO TRUE
+               EXEC SQL COMMIT WORK END-EXEC
+               PERFORM 0300-LG-DEB
+                  THRU 0300-LG-FIN
+           ELSE 
+               SET LK-AJU-RET-ERR TO TRUE
+               EXEC SQL ROLLBACK WORK END-EXEC
+           END-IF.
+       0200-SQL-FIN.
+
+       0300-LG-DEB.
+           MOVE "ajout d'une livraison" TO WS-DTL-LG.
+           MOVE "utilisateur" TO WS-TYP-LG.
+           CALL "crelog" USING WS-DTL-LG,
+                               WS-TYP-LG,
+                               WS-UTI-ID
+           END-CALL.
+       0300-LG-FIN.
