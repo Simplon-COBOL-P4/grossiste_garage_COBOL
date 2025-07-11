451bfee6f1895342bd545f7d6eadecf455b3efe9
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-11 12:06:31 +0200

    [ajout] Création structure

---
+++ srcs/livraison/lirpgliv.cbl
@@ -0,0 +1,55 @@
+      ******************************************************************
+      *                             ENTÊTE                             *
+      *                                                                *
+      *                                                                *
+      *                                                                *
+      *                           TRIGRAMMES                           *
+      * LIR=LIRE; PG=PAGE; LIV=LIVRAISON;                              *
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. lirpgliv.
+       AUTHOR. .
+       DATE-WRITTEN. .
+
+       DATA DIVISION.
+       LINKAGE SECTION.
+      * Arguments d'entrée.
+       77 LK-PAGE                         PIC 9(10).
+       77 NOMBRE                          PIC 9(02).
+       77 IDENTIFIANT-FOUR-CLIENT-PIECE   PIC 9(10).
+       01 FILTRE                          PIC 9(01).
+           88 FILTRE-VIDE                           VALUE 0.
+           88 FILTRE-FOURNISSEUR                    VALUE 1.
+           88 FILTRE-CLIENT                         VALUE 2.
+           88 FILTRE-PIECE                          VALUE 3.
+      * Arguments de sortie.
+       01 TABLEAU.
+           05 LIVRAISON OCCURS 25 TIMES.
+               10 IDENTIFIANT-LIVRAISON   PIC 9(10).
+               10 DATE-LIVRAISON          PIC X(10).
+               10 STATUT-LIVRAISON        PIC 9(01).
+                   88 STATUT-EN-COURS               VALUE 0.
+                   88 STATUS-TERMINE                VALUE 1.
+               10 TYPE-LIVRAISON          PIC 9(01).
+                   88 TYPE-ENTRANT                  VALUE 0.
+                   88 TYPE-SORTANT                  VALUE 1.
+               10 IDENTIFIANT-FOUR-CLIENT PIC 9(10).
+               10 NOM-FOUR-CLIENT         PIC X(50).
+      * Attention à l'ambivalance de cet argument, il peut etre le
+      * nombre de variete de pieces dans une livraison, comme le nombre
+      * de piece de l'ID demandé dans la livraison. 
+               10 QUANTITE-PIECE          PIC 9(10).
+
+       COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
+
+       PROCEDURE DIVISION USING LK-PAGE,
+                                NOMBRE,
+                                IDENTIFIANT-FOUR-CLIENT-PIECE,
+                                FILTRE,
+                                TABLEAU,
+                                WS-LIR-RET.
+
+           EXIT PROGRAM.
