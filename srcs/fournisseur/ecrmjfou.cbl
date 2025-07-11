c3b6672674e1f11d40f8b51108832c7a1c726362
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-11 16:45:23 +0200

    [fix] Rajout boucle pour permettre de mettre à jour plusieurs fournisseurs

--- srcs/fournisseur/ecrmjfou.cbl
+++ srcs/fournisseur/ecrmjfou.cbl
@@ -12,6 +12,8 @@
       *                           TRIGRAMMES                           *
       * ADR=Adresse                                                    *
       * AFC=Afficher                                                   *
+      * ARG=Argument                                                   *
+      * BCL=Boucle                                                     *
       * CDP=Code Postale                                               *
       * CHO=Choix                                                      *
       * ECR=ECRAN                                                      *
@@ -24,6 +26,8 @@
       * MJ =MISE A JOUR                                                *
       * MOD=Modifier                                                   *
       * NOM=Nom                                                        *
+      * PRN=Principale                                                 *
+      * RCH=Rechercher                                                 *
       * SSI=Saisi                                                      *
       * TEL=Téléphone                                                  *
       * Vil=Ville                                                      *
@@ -39,6 +43,7 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 WS-IDT-FOU               PIC 9(10).
+       01 WS-IDT-FOU-ARG           PIC 9(10).
        01 WS-NOM-FOU               PIC X(50).
        01 WS-ADR-FOU               PIC X(50).
        01 WS-VIL-FOU               PIC X(50).
@@ -51,7 +56,10 @@
        COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
        COPY majret REPLACING ==:PREFIX:== BY ==WS==.
 
-       77 WS-CHO                   PIC X(01).
+       01 WS-CHO                   PIC 9(01).
+           88 WS-CHO-ANN                     VALUE 0.
+           88 WS-CHO-MOD                     VALUE 1.
+           88 WS-CHO-RCH                     VALUE 2.
        77 WS-ERR                   PIC X(01).
 
        SCREEN SECTION.
@@ -80,7 +88,8 @@
            05 LINE 19 COL 03 VALUE '['.
            05 LINE 19 COL 09 VALUE ']'.
            05 LINE 21 COL 29 VALUE 'Confirmer modifications ?'.
-           05 LINE 22 COL 32 VALUE '1 - Oui   0 - Annuler'.
+           05 LINE 22 COL 24
+               VALUE '1 - Modifier   2 - Rechercher   0 - Annuler'.
            05 LINE 23 COL 39 VALUE '[ ]'.
 
        01  S-SSI-IDT-FOU.
@@ -94,39 +103,39 @@
            05 LINE 15 COL 04 PIC X(50) USING WS-ADR-FOU AUTO.
            05 LINE 17 COL 04 PIC X(50) USING WS-VIL-FOU AUTO.
            05 LINE 19 COL 04 PIC 9(05) USING WS-CDP-FOU AUTO.
-           05 LINE 23 COL 40 PIC X(01) TO WS-CHO.
+           05 LINE 23 COL 40 PIC 9(01) TO WS-CHO.
 
        PROCEDURE DIVISION.
 
-           PERFORM 0100-AFC-ECR-DEB
-              THRU 0100-AFC-ECR-FIN.
-           PERFORM 0200-SSI-IDT-FOU-DEB
-              THRU 0200-SSI-IDT-FOU-FIN.
-           PERFORM 0300-FET-INF-FOU-DEB
-              THRU 0300-FET-INF-FOU-FIN.
-           PERFORM 0400-MOD-INF-FOU-DEB
-              THRU 0400-MOD-INF-FOU-FIN.
+           PERFORM 0100-BCL-PRN-DEB
+              THRU 0100-BCL-PRN-FIN.
+           
            EXIT PROGRAM.
 
+       0100-BCL-PRN-DEB.
+           PERFORM UNTIL WS-CHO-ANN
+
+               PERFORM 0200-AFC-ECR-DEB
+                  THRU 0200-AFC-ECR-FIN
+
+               PERFORM 0400-MOD-INF-FOU-DEB
+                  THRU 0400-MOD-INF-FOU-FIN
+
+           END-PERFORM.
+       0100-BCL-PRN-FIN.
+
       * Affiche l'ecran de saisi
-       0100-AFC-ECR-DEB.    
+       0200-AFC-ECR-DEB.    
            DISPLAY S-FND-ECR.
            DISPLAY S-SSI-FOU.
-       0100-AFC-ECR-FIN.
-           EXIT.
-           
-       0200-SSI-IDT-FOU-DEB.
-           PERFORM UNTIL WS-IDT-FOU <> 0
-               ACCEPT S-SSI-IDT-FOU
-           END-PERFORM.
-       0200-SSI-IDT-FOU-FIN.
+       0200-AFC-ECR-FIN.
            EXIT.
 
        0300-FET-INF-FOU-DEB.
            CALL "liridfou"
                USING
       * Arguments d'entree.
-               WS-IDT-FOU
+               WS-IDT-FOU-ARG
       * Arguments de sortie.
                WS-NOM-FOU
                WS-ADR-FOU
@@ -138,53 +147,60 @@
                WS-LIR-RET
            END-CALL.
 
-           IF  WS-LIR-RET <> 0 THEN
+           IF WS-LIR-RET-ERR
                DISPLAY 'Fournisseur non trouve' AT LINE 23 COL 2
                ACCEPT WS-ERR AT LINE 23 COL 24
-               EXIT PROGRAM
            END-IF.
 
        0300-FET-INF-FOU-FIN.
            EXIT.
 
        0400-MOD-INF-FOU-DEB.
-           PERFORM UNTIL WS-CHO = '1' OR '0'
-               ACCEPT S-MOD-INF-FOU
-               EVALUATE WS-CHO
-                   WHEN '1'
-                       CALL "majfou"
-                           USING
-                           WS-IDT-FOU
-                           WS-NOM-FOU
-                           WS-ADR-FOU
-                           WS-VIL-FOU
-                           WS-CDP-FOU
-                           WS-ITL-FOU
-                           WS-TEL-FOU
-                           WS-EML-FOU
-                           WS-MAJ-RET
-                       END-CALL
-                       IF  WS-MAJ-RET <> 0 THEN
-                           DISPLAY 'Echec de la mise a jour' AT LINE 23
-                                                                   COL 2
-                           ACCEPT WS-ERR AT LINE 23 COL 25
-                           EXIT PROGRAM
-                       ELSE 
-                           DISPLAY 'Mise a jour terminee' AT LINE 23 COL
-                                                                       2
-                           ACCEPT WS-ERR AT LINE 23 COL 22
-                       END-IF
-                   WHEN '0'
-                       DISPLAY 'Mise a jour annulee' AT LINE 23 COL 2
-                       ACCEPT WS-ERR AT LINE 23 COL 21
-                   WHEN OTHER
-                       DISPLAY "Veuillez confirmer par '1' ou '0'" AT 
-                                                           LINE 23 COL 2
-                       ACCEPT WS-ERR AT LINE 23 COL 35
-                       DISPLAY '                                  ' AT
-                                                           LINE 23 COL 2
-               END-EVALUATE
-           END-PERFORM. 
+           ACCEPT S-MOD-INF-FOU
+           EVALUATE TRUE
+               WHEN WS-CHO-ANN
+                   DISPLAY 'Retour au menu precedent'
+                       AT LINE 23 COL 2
+                   ACCEPT WS-ERR
+                       AT LINE 23 COL 21
+               WHEN WS-CHO-MOD
+                   CALL "majfou"
+                       USING
+                       WS-IDT-FOU-ARG
+                       WS-NOM-FOU
+                       WS-ADR-FOU
+                       WS-VIL-FOU
+                       WS-CDP-FOU
+                       WS-ITL-FOU
+                       WS-TEL-FOU
+                       WS-EML-FOU
+                       WS-MAJ-RET
+                   END-CALL
+                   IF  WS-MAJ-RET <> 0 THEN
+                       DISPLAY 'Echec de la mise a jour'
+                           AT LINE 23 COL 2
+                       ACCEPT WS-ERR
+                           AT LINE 23 COL 25
+                       EXIT PROGRAM
+                   ELSE 
+                       DISPLAY 'Mise a jour terminee'
+                           AT LINE 23 COL 2
+                       ACCEPT WS-ERR
+                           AT LINE 23 COL 22
+                   END-IF
+               
+               WHEN WS-CHO-RCH
+                   MOVE WS-IDT-FOU TO WS-IDT-FOU-ARG
+                   PERFORM 0300-FET-INF-FOU-DEB
+                      THRU 0300-FET-INF-FOU-FIN
 
+               WHEN OTHER
+                   DISPLAY "Entrez 1, 2, ou 0."
+                       AT LINE 23 COL 2
+                   ACCEPT WS-ERR
+                       AT LINE 23 COL 35
+                   DISPLAY '                                  '
+                       AT LINE 23 COL 2
+           END-EVALUATE.
        0400-MOD-INF-FOU-FIN.
            EXIT.
