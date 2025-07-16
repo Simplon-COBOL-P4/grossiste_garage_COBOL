3797c0da98bb296232101debfe006a50c3093be8
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-16 05:37:11 +0200

    [modif] Changement logique boucle + nettoyage + rajout option pour quitter

--- srcs/connexion/conapl.cbl
+++ srcs/connexion/conapl.cbl
@@ -14,6 +14,7 @@
       *  DTL = Detail
       *  ECR = Ecran
       *  EFC = Efface
+      *  ENC = En cours
       *  ERR = Erreur
       *  FND = Fond
       *  GBL = Global
@@ -41,7 +42,7 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. conapl.
-       AUTHOR.    Benoit.
+       AUTHOR. Benoit.
        DATE-WRITTEN. 25-06-2025 (fr).
 
       ******************************************************************
@@ -64,13 +65,12 @@
        01  WS-ID-UTL            PIC 9(10).
        01  WS-RLE-UTL           PIC X(14).
 
+       01  WS-CHX               PIC X(01) VALUE SPACE.
+           88 WS-CHX-VID                  VALUE " ".
 
-       77  WS-CDR-005           PIC X(13) VALUE 'Identifiant :'.
-
-       77  WS-CDR-006           PIC X(01) VALUE '['.
-       77  WS-CDR-007           PIC X(01) VALUE ']'.
-
-       77  WS-CDR-008           PIC X(14) VALUE 'Mot de passe :'.
+       01 WS-ETT-BCL            PIC 9(01).
+           88 WS-ETT-BCL-ENC              VALUE 1.
+           88 WS-ETT-BCL-FIN              VALUE 2.
 
        COPY ajuret REPLACING ==:PREFIX:== BY ==WS==.
        COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
@@ -81,21 +81,30 @@
        COPY ecrprn.
 
        01  ECR-SSI-01.
-           05  LINE 7  COL 34  PIC X(13) FROM WS-CDR-005.
-           05  LINE 9  COL 29  PIC X(01) FROM WS-CDR-006.
-           05  LINE 9  COL 50  PIC X(01) FROM WS-CDR-007.
-           05  LINE 13 COL 29  PIC X(01) FROM WS-CDR-006.
-           05  LINE 13 COL 50  PIC X(01) FROM WS-CDR-007.
+           05  LINE 7  COL 34  PIC X(13) VALUE 'Identifiant :'.
+
+           05  LINE 9  COL 29  PIC X(01) VALUE "[".
+           05  LINE 9  COL 50  PIC X(01) VALUE "]".
+
+           05  LINE 11 COL 33  PIC X(13) VALUE 'Mot de passe :'.
+
+           05  LINE 13 COL 29  PIC X(01) VALUE "[".
+           05  LINE 13 COL 50  PIC X(01) VALUE "]".
+
+           05  LINE 23.
+               10 COL 62 VALUE "Retour au menu".
+               10 COL 77 VALUE "[".
+               10 COL 78 USING PIC X(01) WS-CHX.
+               10 COL 79 VALUE "]".
+           
            05  LINE 9  COL 30  PIC X(20) TO WS-NOM-UTL AUTO.
            05  LINE 13 COL 30  PIC X(20) TO WS-MDP-UTL SECURE AUTO
                FOREGROUND-COLOR WS-COL-TXT
                BACKGROUND-COLOR WS-COL-FND.
 
        01  ECR-SSI-02.
-           05 LINE 22 COL 10   VALUE
-                         "Identifiant et/ou mot de passe incorrecte".
-           05 LINE 23 COL 10   VALUE "Nombre de tentative restant: ".
-           05 LINE 23 COL 39   PIC X(01) FROM WS-NBR-RST.
+           05 LINE 22 COL 10 
+               VALUE "Identifiant et/ou mot de passe incorrecte".
 
       ******************************************************************
       *
@@ -108,76 +117,91 @@
            EXIT PROGRAM.
 
        0100-CON-DEB.
-           SET LK-STT-ERR TO TRUE.
            DISPLAY S-FND-ECR
-           DISPLAY 'Nombre de tentative restant: 03' AT LINE 23 COL 10
-           PERFORM VARYING WS-NBR-CON FROM 1 BY 1 UNTIL WS-NBR-CON > 3 
-                   OR NOT LK-STT-ERR
-               DISPLAY ECR-SSI-01
+           MOVE SPACE TO WS-CHX.
+           SET WS-ETT-BCL-ENC TO TRUE.
+           PERFORM UNTIL WS-ETT-BCL-FIN
+
                ACCEPT ECR-SSI-01
-      * Appel sous-progrmme
-               CALL "liruti"
-                   USING
-                   WS-NOM-UTL
-                   WS-MDP-UTL
-                   WS-ROL-UTL
-                   WS-ID-UTL
-                   WS-LIR-RET
-               END-CALL
-               IF WS-LIR-RET-ERR THEN
-                   DISPLAY 'Identifiant et/ou mot de passe incorrecte'
-                       AT LINE 22 COL 10
-                   DISPLAY 'Nombre de tentative restant: ' 
-                       AT LINE 23 COL 10
+               
+               EVALUATE TRUE 
+                   WHEN WS-CHX-VID
+                       PERFORM 0150-LGQ-PRN-DEB
+                          THRU 0150-LGQ-PRN-FIN
 
-                   COMPUTE WS-NBR-RST = 3 - WS-NBR-CON
+                   WHEN OTHER
+                       SET WS-ETT-BCL-FIN TO TRUE
 
-                   DISPLAY WS-NBR-RST 
-                       AT LINE 23 COL 39
+               END-EVALUATE
 
-                   MOVE "CON_ERR" TO WS-TYP-LG
-                   MOVE SPACE TO WS-DTL-LG
-                   STRING
-                       FUNCTION TRIM(WS-NOM-UTL) SPACE
-                       WS-NBR-RST " restants."
-                       DELIMITED BY SIZE 
-                       INTO WS-DTL-LG 
-                   END-STRING
-                   MOVE 0 TO WS-ID-UTL
-                   CALL "ajulog"
-                       USING
-                       WS-DTL-LG
-                       WS-TYP-LG
-                       WS-ID-UTL
-                       WS-AJU-RET
-                   END-CALL
-               ELSE
-                   PERFORM 0200-DEP-VER-GBL-DEB
-                      THRU 0200-DEP-VER-GBL-FIN
-                   MOVE "CON_SUC" TO WS-TYP-LG
-                   MOVE SPACE TO WS-DTL-LG
-                   STRING
-                       FUNCTION TRIM(WS-NOM-UTL)
-                       " connecte."
-                       DELIMITED BY SIZE 
-                       INTO WS-DTL-LG 
-                   END-STRING
-                   CALL "ajulog"
-                       USING
-                       WS-DTL-LG
-                       WS-TYP-LG
-                       WS-ID-UTL
-                       WS-AJU-RET
-                   END-CALL
-               END-IF
            END-PERFORM.
 
        0100-CON-FIN.
-       EXIT.
+           DISPLAY S-FND-ECR.
+      
+           CALL "liruti"
+               USING
+               WS-NOM-UTL
+               WS-MDP-UTL
+               WS-ROL-UTL
+               WS-ID-UTL
+               WS-LIR-RET
+           END-CALL.
+
+           IF WS-LIR-RET-ERR THEN
+               DISPLAY 'Identifiant et/ou mot de passe incorrecte'
+                   AT LINE 22 COL 10
+
+               PERFORM 0300-DTL-LOG-ERR-DEB
+                  THRU 0300-DTL-LOG-ERR-FIN
+
+               MOVE "CON_ERR" TO WS-TYP-LG
+               MOVE 0 TO WS-ID-UTL
+           ELSE
+               PERFORM 0200-DEP-VER-GBL-DEB
+                  THRU 0200-DEP-VER-GBL-FIN
+               
+               PERFORM 0400-DTL-LOG-SUC-DEB
+                  THRU 0400-DTL-LOG-SUC-FIN
+
+               MOVE "CON_SUC" TO WS-TYP-LG
+           END-IF.
+
+           CALL "ajulog"
+               USING
+               WS-DTL-LG
+               WS-TYP-LG
+               WS-ID-UTL
+               WS-AJU-RET
+           END-CALL.
+       0150-LGQ-PRN-DEB.
+
+       0150-LGQ-PRN-FIN.
       
        0200-DEP-VER-GBL-DEB.
            MOVE WS-NOM-UTL TO G-UTI-NOM.
            MOVE WS-ID-UTL TO G-UTI-ID.
            MOVE WS-RLE-UTL TO G-UTI-RLE.
-       0200-DEP-VER-GBL-FIN.    
+       0200-DEP-VER-GBL-FIN.
 
+       0300-DTL-LOG-ERR-DEB.
+           MOVE SPACE TO WS-DTL-LG
+
+           STRING
+               FUNCTION TRIM(WS-NOM-UTL) SPACE
+               WS-NBR-RST " restants."
+               DELIMITED BY SIZE 
+               INTO WS-DTL-LG 
+           END-STRING.
+       0300-DTL-LOG-ERR-FIN.
+
+       0400-DTL-LOG-SUC-DEB.
+           MOVE SPACE TO WS-DTL-LG
+
+           STRING
+               FUNCTION TRIM(WS-NOM-UTL)
+               " connecte."
+               DELIMITED BY SIZE 
+               INTO WS-DTL-LG 
+           END-STRING.
+       0400-DTL-LOG-SUC-FIN.
