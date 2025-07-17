9a272a7c1632ccb59024417dea69066745dca2a6
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-17 09:52:04 +0200

    [nettoyage] Manque retour erreur FK

--- srcs/livraison/ajulivpi.cbl
+++ srcs/livraison/ajulivpi.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *                           TRIGRAMMES                           *
       * AJU=AJOUT; DEP=DEPLACER; IDF=IDENTIFIANT; LIV=LIVRAISON;       *
-      * PI=PIECE; QTE=QUANTITE; REQ=REQUÊTE; VAR=VARIABLE;             *
+      * PIE,PI=PIECE; QTE=QUANTITE; REQ=REQUÊTE; VAR=VARIABLE;         *
       *                                                                *
       ******************************************************************
        
@@ -19,59 +19,60 @@
        WORKING-STORAGE SECTION.
        EXEC SQL BEGIN DECLARE SECTION END-EXEC.
        01 PG-IDF-LIV              PIC 9(10).
-       01 PG-IDF-PI               PIC 9(10).
-       01 PG-QTE-PI               PIC 9(10).
+       01 PG-IDF-PIE               PIC 9(10).
+       01 PG-QTE-PIE               PIC 9(10).
        EXEC SQL END DECLARE SECTION END-EXEC.
        EXEC SQL INCLUDE SQLCA END-EXEC.
 
        LINKAGE SECTION.
       * Arguments d'entrée.
        01 LK-IDF-LIV              PIC 9(10).
-       01 LK-IDF-PI               PIC 9(10).
-       01 LK-QTE-PI               PIC 9(10).
+       01 LK-IDF-PIE               PIC 9(10).
+       01 LK-QTE-PIE               PIC 9(10).
       * Arguments de sortie.
 
        COPY ajuret REPLACING ==:PREFIX:== BY ==LK==.
 
        PROCEDURE DIVISION USING LK-IDF-LIV,
-                                LK-IDF-PI,
-                                LK-QTE-PI,
+                                LK-IDF-PIE,
+                                LK-QTE-PIE,
                                 LK-AJU-RET.
 
 
-           PERFORM 0100-AJU-LIV-PI-DEB
-              THRU 0100-AJU-LIV-PI-FIN.
+           PERFORM 0100-AJU-LIV-PIE-DEB
+              THRU 0100-AJU-LIV-PIE-FIN.
 
            EXIT PROGRAM.
            
 
       ****************************PARAGRAPHES***************************
-       0100-AJU-LIV-PI-DEB.
+       0100-AJU-LIV-PIE-DEB.
 
        0110-DEP-VAR-DEB.
            MOVE LK-IDF-LIV   TO PG-IDF-LIV.
-           MOVE LK-IDF-PI    TO PG-IDF-PI.
-           MOVE LK-QTE-PI    TO PG-QTE-PI.
+           MOVE LK-IDF-PIE    TO PG-IDF-PIE.
+           MOVE LK-QTE-PIE    TO PG-QTE-PIE.
        0110-DEP-VAR-FIN.
 
-       0120-REQ-SQL-DEB.    
-       EXEC SQL
-           INSERT INTO livraison_piece (id_liv, id_pie, qt_liv_pie)
-           VALUES (:PG-IDF-LIV, :PG-IDF-PI, :PG-QTE-PI)
-       END-EXEC.
+       0120-REQ-SQL-DEB.
+
+           EXEC SQL
+               INSERT INTO livraison_piece (id_liv, id_pie, qt_liv_pie)
+               VALUES (:PG-IDF-LIV, :PG-IDF-PIE, :PG-QTE-PIE)
+           END-EXEC.
   
-           IF SQLCODE = 0 THEN
-
-               SET LK-AJU-RET-OK TO TRUE
-
-               EXEC SQL COMMIT END-EXEC
-
-           ELSE 
-           
-               SET LK-AJU-RET-ERR TO TRUE
-
-           END-IF.
+           EVALUATE SQLCODE
+               WHEN 0
+                   SET LK-AJU-RET-OK TO TRUE
+                   EXEC SQL COMMIT END-EXEC
+               WHEN -400
+                   SET LK-AJU-RET-FK-ERR TO TRUE
+                   EXEC SQL ROLLBACK END-EXEC
+               WHEN OTHER
+                   SET LK-AJU-RET-ERR TO TRUE
+                   EXEC SQL ROLLBACK END-EXEC
+           END-EVALUATE.
        0120-REQ-SQL-FIN.    
        
-       0100-AJU-LIV-PI-FIN.
+       0100-AJU-LIV-PIE-FIN.
            
