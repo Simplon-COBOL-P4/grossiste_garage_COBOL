962f830745e361873f2dc26c36c2e56f39ddbdf7
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-07-18 07:41:13 +0200

    [fix] Écran log qui marche

--- srcs/log/front/ecrpglog.cbl
+++ srcs/log/front/ecrpglog.cbl
@@ -66,7 +66,7 @@
            
 
       *Déclaration des variables indépendantes.
-       77  WS-MAX-LIN           PIC 9(02) VALUE 6.
+       77  WS-MAX-LIN           PIC 9(02) VALUE 5.
        77  WS-QTT-LIN           PIC 9(02).
        77  WS-LIN-IDX           PIC 9(02).
        77  WS-IDX               PIC 9(02).
@@ -78,7 +78,16 @@
            88 WS-ETT-BCL-ENC              VALUE 1.
            88 WS-ETT-BCL-FIN              VALUE 2.
 
+       01 WS-MNU-2                PIC 9(01).
+           88 WS-MNU-2-INV                  VALUE 1.
+           88 WS-MNU-2-VIS                  VALUE 2.
+
+       01 WS-ERR-SQL            PIC X(76) VALUE
+           "Une erreur est survenue durant la requete".
+
        COPY lirret REPLACING ==:PREFIX:== BY ==WS==.
+
+       COPY ctxerr.
 
        COPY utiglb.
 
@@ -93,17 +102,16 @@
            05 LINE 23.
                10 COL 03 VALUE "Choix de la page".
                10 COL 20 VALUE "[".
-               10 COL 21 PIC 9(10) USING WS-NUM-PAG.
+               10 COL 21 PIC Z(10) USING WS-NUM-PAG.
                10 COL 31 VALUE "]".
                10 COL 62 VALUE "Retour au menu".
                10 COL 77 VALUE "[".
                10 COL 78 PIC X(01) USING WS-CHX.
                10 COL 79 VALUE "]".
+           05 LINE 22 COL 02 FROM WS-UND-LIN.
 
-       01 S-AFF-ERR.
-           05 LINE 18.
-               10 COL 03 VALUE
-                   "Une erreur est survenue durant la requete".
+       01 S-MSG-ERR.
+           05 LINE 18 COLUMN 03 FROM WS-MSG-ERR.
 
 
        PROCEDURE DIVISION.
@@ -117,23 +125,29 @@
       ******************************************************************
 
        0050-BCL-DEB.
-           DISPLAY S-FND-ECR.
            SET WS-ETT-BCL-ENC TO TRUE.
+           SET WS-MNU-2-INV TO TRUE.
            PERFORM UNTIL WS-ETT-BCL-FIN
+               DISPLAY S-FND-ECR
+
+               PERFORM 0500-AFF-ERR-CND-DEB
+                  THRU 0500-AFF-ERR-CND-FIN
+
+               PERFORM 0400-AFF-MNU-2-CND-DEB
+                  THRU 0400-AFF-MNU-2-CND-FIN
+
                ACCEPT S-MNU-LOG
                EVALUATE TRUE
                    WHEN WS-CHX-VID
-                       DISPLAY S-FND-ECR
       * Appel d'un sous-programme pour récupérer les logs en bdd.
                        PERFORM 0100-APL-LEC-LOG-DEB
                           THRU 0100-APL-LEC-LOG-FIN
                        IF WS-LIR-RET-OK THEN
-      * Affichages des lignes avec les logs.  
-                           PERFORM 0300-AFF-LOG-DEB
-                              THRU 0300-AFF-LOG-FIN
+      * Affichages des lignes avec les logs.
+                           SET WS-MNU-2-VIS TO TRUE 
                        ELSE
-                           PERFORM 0200-AFF-ERR-DEB
-                              THRU 0200-AFF-ERR-FIN
+                           PERFORM 0600-ERR-SQL-DEB
+                              THRU 0600-ERR-SQL-FIN
                        END-IF
                    WHEN OTHER
                        SET WS-ETT-BCL-FIN TO TRUE
@@ -153,12 +167,8 @@
            END-CALL.
        0100-APL-LEC-LOG-FIN.
 
-       0200-AFF-ERR-DEB.
-           DISPLAY S-AFF-ERR.
-       0200-AFF-ERR-FIN.
-
        0300-AFF-LOG-DEB. 
-           MOVE 10 TO WS-LIN-IDX.
+           MOVE 12 TO WS-LIN-IDX.
            
            PERFORM VARYING WS-IDX FROM 1 BY 1 UNTIL WS-IDX > WS-QTT-LIN
 
@@ -177,3 +187,22 @@
            END-PERFORM.
        0300-AFF-LOG-FIN.
        
+       0400-AFF-MNU-2-CND-DEB.
+           IF WS-MNU-2-VIS THEN
+               PERFORM 0300-AFF-LOG-DEB
+                  THRU 0300-AFF-LOG-FIN
+               SET WS-MNU-2-INV TO TRUE
+           END-IF.
+       0400-AFF-MNU-2-CND-FIN.
+
+       0500-AFF-ERR-CND-DEB.
+           IF WS-CTX-AFF-ERR THEN
+               DISPLAY S-MSG-ERR
+               SET WS-CTX-OK TO TRUE
+           END-IF.
+       0500-AFF-ERR-CND-FIN.
+
+       0600-ERR-SQL-DEB.
+           SET WS-CTX-AFF-ERR TO TRUE.
+           MOVE WS-ERR-SQL TO WS-MSG-ERR.
+       0600-ERR-SQL-FIN.
